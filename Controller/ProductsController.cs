eacddeefb7861ce090878c5b58a4a4d6fab91b4e
christianjavier25 <58329959+christianjavier25@users.noreply.github.com>
2019-12-03 19:38:52 +0800

    Initial Commit

---
+++ Controller/ProductsController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using JavierIP.WebSite.Models;
+using JavierIP.WebSite.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JavierIP.WebSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public JsonFileProductService ProductService { get; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return ProductService.GetProducts();
+        }
+
+        //[HttpPatch] "[FromBody]"
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get(
+           [FromQuery] string ProductId,
+           [FromQuery] int Rating)
+        {
+            ProductService.AddRating(ProductId,Rating);
+
+            return Ok();
+        }
+
+        
+    }
+}
